fd54e083c1a369ef2771d81c63950b3c59476136
lab_services_student <lab_services_student@labVMH8OX>
2024-07-24 17:17:28 +0200

    Added Student Report

---
+++ XBCADAttendance/Models/StudentReportViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace XBCADAttendance.Models
+{
+    public class StudentReportViewModel
+    {
+        public string ModuleCode { get; set; }
+        public DateTime LectureDate { get; set; }
+        public string ClassroomCode { get; set; }
+        public DateTime ScanIn { get; set; }
+        public DateTime ScanOut { get; set; }
+    }
+}
