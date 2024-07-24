12ecc2e43b4373b1f8eb0b9c14cc0741158d522e
JarrydHarker <st10209687@vcconnect.edu.za>
2024-07-24 16:24:06 +0200

    Connected Controller to DB

---
+++ XBCADAttendance/Models/DataAccess.cs
@@ -0,0 +1,53 @@
+﻿namespace XBCADAttendance.Models
+{
+    public class DataAccess
+    {
+        public DbWilContext context = new DbWilContext();
+        public static DataAccess instance = null;
+        
+        public DataAccess()
+        { 
+            
+        }
+
+        public static DataAccess GetContext()
+        {
+            if (instance == null)
+            {
+                instance = new DataAccess();
+            }
+
+            return instance;
+        }
+
+        public string AddStudent(string UserID, string studentNo, string Username, string Password)
+        {
+            try
+            {
+                TblUser newUser = new TblUser
+                {
+                    UserId = UserID,
+                    UserName = Username,
+                    Password = Password
+                    
+                };
+
+                TblStudent newStudent = new TblStudent
+                {
+                    StudentNo = studentNo,
+                    UserId = UserID,
+                };
+
+                context.TblUsers.Add(newUser);
+                context.TblStudents.Add(newStudent);
+                context.SaveChanges();
+
+                return "Success";
+            
+            }catch (Exception e)
+            {
+                return e.ToString();
+            }
+        }
+    }
+}
