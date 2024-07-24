691776cdcb10d3f75eb536c6b785c24104460930
lab_services_student <St10132642@vcconnect.edu.za>
2024-07-24 15:43:38 +0200

    added everything that i did in the previous one all from styling to adding the sidebars and login

---
+++ XBCADAttendance/wwwroot/js/service-worker.js
@@ -0,0 +1,23 @@
+﻿self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open('static-cache').then(function (cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/script.js',
+                '/manifest.json',
+                '/icons/instagram (1).png',
+                '/icons/instagram.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
