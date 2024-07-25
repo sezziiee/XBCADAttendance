5b686d6e57a45eb88fbab884283090c581c2005f
Leon <103996275+LeonPotgiter@users.noreply.github.com>
2024-07-25 08:56:46 +0200

    added lecture report

---
+++ XBCADAttendance/Controllers/LectureReportController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using XBCADAttendance.Models;
+
+namespace XBCADAttendance.Controllers
+{
+	public class LectureReportController : Controller
+	{
+		List<LecturerReportViewModel> list = new List<LecturerReportViewModel>();
+
+		public IActionResult Index()
+		{
+			var list = new LecturerReportViewModel("st10185639", "shap", "Y", 0);
+			return View(list);
+		}
+	}
+}
