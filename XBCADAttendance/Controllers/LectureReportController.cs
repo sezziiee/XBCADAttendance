8213f1383739f23aa18d876defff2c028f3fab7e
Leon <103996275+LeonPotgiter@users.noreply.github.com>
2024-07-25 09:28:42 +0200

    changed

--- XBCADAttendance/Controllers/LectureReportController.cs
+++ XBCADAttendance/Controllers/LectureReportController.cs
@@ -5,12 +5,13 @@ namespace XBCADAttendance.Controllers
 {
 	public class LectureReportController : Controller
 	{
-		List<LecturerReportViewModel> list = new List<LecturerReportViewModel>();
+		List<LecturerReportViewModel> lists = new List<LecturerReportViewModel>();
 
 		public IActionResult Index()
 		{
-			var list = new LecturerReportViewModel("st10185639", "shap", "Y", 0);
-			return View("LectureReport", list);
+			var list = new LecturerReportViewModel("st10185639", "shap", "Y", 8);
+			lists.Add(list);
+			return View("LectureReport", lists);
 		}
 	}
 }
