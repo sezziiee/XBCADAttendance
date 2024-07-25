b0145f5033cd4fa4e87764586ba3adbdfe94ff7f
JarrydHarker <st10209687@vcconnect.edu.za>
2024-07-25 13:29:27 +0200

    Added UpdateStudent Method

--- XBCADAttendance/Models/DataAccess.cs
+++ XBCADAttendance/Models/DataAccess.cs
@@ -4,10 +4,10 @@
     {
         public DbWilContext context = new DbWilContext();
         public static DataAccess instance = null;
-        
+
         public DataAccess()
-        { 
-            
+        {
+
         }
 
         public static DataAccess GetContext()
@@ -53,5 +53,103 @@
                 }
             } else return "Please Fill in all fields";
         }
+
+        //Read
+        public List<TblStudent> GetAllStudents()
+        {
+            var data = context.TblStudents.ToList();
+
+            if (data != null)
+            {
+                return data;
+            } else return null;
+        }
+
+        public List<TblLecture> GetAllLectures()
+        {
+            var data = context.TblLectures.ToList();
+
+            if (data != null)
+            {
+                return data;
+            } else return null;
+        }
+
+        public List<TblModule> GetAllModules()
+        {
+            var data = context.TblModules.ToList();
+
+            if (data != null)
+            {
+                return data;
+            } else return null;
+        }
+
+        public List<TblUser> GetAllUsers()
+        {
+            var data = context.TblUsers.ToList();
+
+            if (data != null)
+            {
+                return data;
+            } else return null;
+        }
+
+        public List<TblStaff> GetAllStaff()
+        {
+            var data = context.TblStaffs.ToList();
+
+            if (data != null)
+            {
+                return data;
+            } else return null;
+        }
+
+        public List<TblRole> GetAllRoles()
+        {
+            var data = context.TblRoles.ToList();
+
+            if (data != null)
+            {
+                return data;
+            } else return null;
+        }
+
+        //Update
+        public string UpdateStudent(string userID, string? studentNo, string? userName, string? passWord)
+        {
+            bool updateName = false;
+            bool updatePassword = false;
+
+            if (userName != null)
+            {
+                updateName = true;
+            }
+
+            if (passWord != null)
+            {
+                updatePassword = true;
+            }
+
+            if (updateName && updatePassword)
+            {
+                var student = context.TblStudents.Where(x => x.StudentNo == studentNo).FirstOrDefault();
+
+                student.User.UserName = userName;
+                student.User.Password = passWord;
+
+            }else if(updateName && !updatePassword)
+            {
+
+            }else if(!updateName && updatePassword)
+            {
+
+            } else
+            {
+                return "No values were updated";
+            }
+        }
+
+        //Delete
     }
 }
