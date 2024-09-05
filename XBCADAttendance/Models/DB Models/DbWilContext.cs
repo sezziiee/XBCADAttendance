982a3b2cec0070dfaff8395ce094fb4ada13b7b5
JarrydHarker <st10209687@vcconnect.edu.za>
2024-09-05 19:09:25 +0200

    Quick fix

--- XBCADAttendance/Models/DB Models/DbWilContext.cs
+++ XBCADAttendance/Models/DB Models/DbWilContext.cs
@@ -22,15 +22,9 @@ namespace XBCADAttendance
 
     public virtual DbSet<TblUser> TblUsers { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer(Environment.GetEnvironmentVariable("CONNECTION_STRING"));
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<TblModule>(entity =>
-        {
-        }
+            => optionsBuilder.UseSqlServer(Environment.GetEnvironmentVariable("CONNECTION_STRING"));
 
         public DbWilContext(DbContextOptions<DbWilContext> options)
             : base(options)
