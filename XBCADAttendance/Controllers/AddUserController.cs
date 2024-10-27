c57a8054a41c56942d675d766035d123e8ec5b33
JarrydHarker <st10209687@vcconnect.edu.za>
2024-10-27 17:25:09 +0200

    Added Edit Pages

--- XBCADAttendance/Controllers/AddUserController.cs
+++ XBCADAttendance/Controllers/AddUserController.cs
@@ -26,43 +26,5 @@ namespace XBCADAttendance.Controllers
 
             return View(model);
         }
-
-        [HttpGet]
-        public IActionResult AddStaff()
-        {
-            return View(new AddStaffViewModel());
-        }
-        
-        [HttpPost]
-        public IActionResult AddStaff(AddStaffViewModel model)
-        {
-            try
-            {
-                TblUser user = new TblUser
-                {
-                    UserName = model.Name,
-                    UserId = model.UserId,
-                    Password = model.Password
-                };
-
-                TblStaff staff = new TblStaff 
-                { 
-                    UserId = model.UserId,
-                    RoleId = model.RoleID,
-                    StaffId = model.StaffNumber,
-                };
-
-                DataAccess.context.TblUsers.Add(user);
-                DataAccess.context.TblStaffs.Add(staff);
-                DataAccess.context.SaveChanges();
-
-            } catch (Exception ex)
-            {
-                ViewBag.Message = ex.Message;
-                return View(model);
-            }
-
-            return RedirectToAction("Index","LectureReport");
-        }
     }
 }
