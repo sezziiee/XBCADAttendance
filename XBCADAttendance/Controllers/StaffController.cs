4b104ddc7467f9cdd1d01357f4f83648822d9a0d
sezziiee <ST10085210@vcconnect.edu.za>
2024-10-29 19:43:51 +0200

    Cleaned stuff up

--- XBCADAttendance/Controllers/StaffController.cs
+++ XBCADAttendance/Controllers/StaffController.cs
@@ -63,6 +63,27 @@ namespace XBCADAttendance.Controllers
             return RedirectToAction("Index", "Home");
         }
 
+        [Authorize(Policy = "LecturerOnly")]
+        public IActionResult Profile()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                string userID = User.Identity.Name;
+
+                if (!userID.IsNullOrEmpty())
+                {
+                    LectureReportViewModel newModel = new LectureReportViewModel(userID);
+                    return View(newModel);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            return RedirectToAction("Index", "Home");
+
+        }
 
     }
 }
